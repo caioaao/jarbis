4bcf3ce8beca3406db5f3a33755041d67a01f579
Caio <caioaao@gmail.com>
2015-09-22 21:36:36 -0300

    fixing build break

--- simulator/SimMain.cc
+++ simulator/SimMain.cc
@@ -10,13 +10,11 @@
 const unsigned int UI_ROWS_ = 30;
 const unsigned int UI_COLS_ = 30;
 
-std::shared_ptr<simulator::Ui> UI;
+std::shared_ptr<simulator::Ui> UI(new simulator::Ui(UI_ROWS_, UI_COLS_));
 
 
 int main()
 {
-    UI = make_shared<simulator::Ui>(new simulator::Ui(UI_ROWS_, UI_COLS_));
-
     for(;;)
     {
         UI->update();
