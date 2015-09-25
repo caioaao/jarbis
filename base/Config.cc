3adc636794894e272e9cb9b6bf1992a2735e6c7b
Caio <caioaao@gmail.com>
2015-09-24 21:07:23 -0300

    Creating simple config parser

--- base/Config.cc
+++ base/Config.cc
@@ -1,12 +1,57 @@
 #include "base/Config.h"
 
-#include "base/Config.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 namespace cfg
 {
     void
     ConfigMap::add_from_file(std::string filepath)
     {
-        //stub
+        std::ifstream ifs;
+
+        ifs.open(filepath, std::ifstream::in);
+
+        std::string line;
+        while(getline(ifs, line))
+        {
+            parse_line_(line);
+        }
+
+        ifs.close();
+    }
+
+
+    void
+    ConfigMap::parse_line_(std::string line)
+    {
+        std::stringstream linestream;
+        std::string key;
+        int64_t value;
+
+        linestream.str(line);
+
+        linestream >> key >> value;
+
+        if(cfg_dict_.count(key))
+        {
+            // stub
+        }
+        cfg_dict_[key] = value;
+    }
+
+
+    int64_t
+    ConfigMap::get(std::string key)
+    {
+        if(cfg_dict_.count(key))
+        {
+            return cfg_dict_[key];
+        }
+        else
+        {
+            return -1;
+        }
     }
 };
--- base/Config.h
+++ base/Config.h
@@ -7,11 +7,14 @@
 
 namespace cfg
 {
-    class ConfigMap:
-        std::unordered_map<std::string, int64_t>
+    class ConfigMap
     {
+    private:
+        std::unordered_map<std::string, int64_t> cfg_dict_;
+        void parse_line_(std::string line);
     public:
         void add_from_file(std::string filepath);
+        int64_t get(std::string key);
     };
 };
 
