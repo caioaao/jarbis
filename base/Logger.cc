f1e803593c025764c8e2d88d1d6855c8ccad7b6f
Caio <caioaao@gmail.com>
2015-09-29 01:02:42 -0300

    Created logging system.

---
+++ base/Logger.cc
@@ -0,0 +1,54 @@
+#include "base/Logger.h"
+
+namespace base
+{
+
+    LogLevel
+    LogStream::log_level(void)
+    {
+        return log_level_;
+    }
+
+
+    LogLevel
+    LogStream::printable_log_level(void)
+    {
+        return printable_log_level_;
+    }
+
+
+    void
+    LogStream::set_log_level(LogLevel new_level)
+    {
+        log_level_ = new_level;
+    }
+
+
+    void
+    LogStream::set_printable_log_level(LogLevel new_level)
+    {
+        printable_log_level_ = new_level;
+    }
+
+
+    LogStream const&
+    LogStream::operator<<(std::ostream &(*F)(std::ostream&)) const
+    {
+        F(out_);
+        return *this;
+    }
+
+
+    LogStream& set_log_level_(LogStream &stream, LogLevel lv)
+    {
+        stream.set_log_level(lv);
+        return stream;
+    }
+
+
+    LogStreamManip<LogLevel>
+    log_level(LogLevel lv)
+    {
+        return LogStreamManip<LogLevel>(set_log_level_, lv);
+    }
+};
---
+++ base/Logger.h
@@ -0,0 +1,77 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <iostream>
+#include <iomanip>
+
+namespace base
+{
+    enum LogLevel
+    {
+        LOG_INFO,
+        LOG_DEBUG,
+        LOG_CRIT,
+        LOG_ERR,
+        LOG_NONE
+    };
+
+
+    class LogStream;
+
+    template <typename T>
+    class LogStreamManip
+    {
+        typedef LogStream& (*LogStreamManipAction_)(LogStream&, T);
+    public:
+        LogStreamManip(LogStreamManipAction_ action, T val)
+            :action_(action), val_(val){}
+        LogStream& act(LogStream& l) { return action_(l,val_); }
+    private:
+        LogStreamManipAction_ action_;
+        T val_;
+
+    };
+
+    class LogStream
+    {
+    private:
+        std::ostream &out_;
+        LogLevel log_level_;
+        LogLevel printable_log_level_;
+    public:
+        LogStream(std::ostream& _out)
+            : out_(_out),
+              log_level_(LogLevel::LOG_INFO),
+              printable_log_level_(LogLevel::LOG_NONE){ }
+
+        LogLevel log_level(void);
+        LogLevel printable_log_level(void);
+
+        void set_log_level(LogLevel new_level);
+        void set_printable_log_level(LogLevel new_level);
+
+        template <typename T>
+        LogStream& operator<<(LogStreamManip<T> manip)
+            {
+                return manip.act(*this);
+            }
+
+        LogStream const& operator<<(std::ostream &(*F)(std::ostream&)) const;
+
+        template <typename T>
+        LogStream const& operator<<(T const& v) const
+            {
+                if(log_level_ >= printable_log_level_)
+                {
+                    out_<< v;
+                }
+
+                return *this;
+            }
+    };
+
+
+    LogStreamManip<LogLevel> log_level(LogLevel lv);
+};
+
+#endif
